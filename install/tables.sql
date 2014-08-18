5f93a0d1915a2a4f1e360ad1ee80a815fbd37681
Zongmin Lei <leizongmin@gmail.com>
2014-08-18 17:55:06 +0800

    更新数据表结构

--- install/tables.sql
+++ install/tables.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2014 年 08 月 14 日 07:44
+-- 生成日期: 2014 年 08 月 18 日 09:53
 -- 服务器版本: 5.1.36
 -- PHP 版本: 5.2.11
 
@@ -21,11 +21,12 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `app_list` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `key` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `client` text CHARACTER SET utf8 NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `client` text NOT NULL,
   `is_sync` tinyint(4) NOT NULL,
-  `description` text CHARACTER SET utf8 NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
   PRIMARY KEY (`id`),
@@ -33,7 +34,22 @@ CREATE TABLE IF NOT EXISTS `app_list` (
   KEY `is_sync` (`is_sync`),
   KEY `created_at` (`created_at`),
   KEY `updated_at` (`updated_at`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user_connect_list`
+--
+
+CREATE TABLE IF NOT EXISTS `user_connect_list` (
+  `user_id` int(11) NOT NULL,
+  `provider` varchar(50) NOT NULL,
+  `unique_id` varchar(255) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`provider`),
+  KEY `unique_id` (`unique_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -44,14 +60,14 @@ CREATE TABLE IF NOT EXISTS `app_list` (
 CREATE TABLE IF NOT EXISTS `user_history_list` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `type` varchar(1) CHARACTER SET utf8 NOT NULL,
+  `type` varchar(1) NOT NULL,
   `timestamp` int(11) NOT NULL,
-  `client_ip` varchar(15) NOT NULL,
+  `client_ip` varchar(15) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `type` (`type`),
   KEY `timestamp` (`timestamp`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -61,9 +77,9 @@ CREATE TABLE IF NOT EXISTS `user_history_list` (
 
 CREATE TABLE IF NOT EXISTS `user_list` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(40) NOT NULL,
   `is_valid` tinyint(4) NOT NULL,
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
@@ -73,4 +89,4 @@ CREATE TABLE IF NOT EXISTS `user_list` (
   KEY `is_valid` (`is_valid`),
   KEY `created_at` (`created_at`),
   KEY `updated_at` (`updated_at`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
